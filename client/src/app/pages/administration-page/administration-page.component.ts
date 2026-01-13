c98a2462b5f7a6a13677b5181478308ce49e37c0
Thomas Pronovost <thomas.pronovost@oronospolytechnique.com>
2026-01-13 14:27:50 -0500

    Base de la vue d'administration

--- client/src/app/pages/administration-page/administration-page.component.ts
+++ client/src/app/pages/administration-page/administration-page.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+
+const ELEMENT_DATA = [
+  { name: 'Hydrogen', size: 1.0079, mode: 'H', lastEdited: '2024-01-01', image: 'assets/images/hydrogen.png' },
+];
 
 @Component({
   selector: 'app-administration-page',
-  imports: [],
+  imports: [MatCardModule, MatTableModule, MatIconModule, MatCheckboxModule],
   templateUrl: './administration-page.component.html',
   styleUrl: './administration-page.component.scss',
 })
 export class AdministrationPageComponent {
-
+  displayedColumns: string[] = ['image', 'name', 'size', 'mode', 'lastEdited', 'visibility', 'actions'];
+  dataSource = ELEMENT_DATA;
 }
