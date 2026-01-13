bf717328f91760b76b30f8bfe4fe907882229544
Thomas Pronovost <thomas.pronovost@oronospolytechnique.com>
2026-01-13 13:01:24 -0500

    Créer la composante de base d'administration

---
+++ client/src/app/pages/administration-page/administration-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administration-page',
+  imports: [],
+  templateUrl: './administration-page.component.html',
+  styleUrl: './administration-page.component.scss',
+})
+export class AdministrationPageComponent {
+
+}
