7d0d6dae047a20f9805e9fabf6b3edbfffccdeb1
linaksa <lina.aksas223@gmail.com>
2026-01-15 23:22:54 -0500

    feat(vue-initiale): route pour creer une partie

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,9 +3,11 @@ import { enableProdMode, enableProfiling, provideZoneChangeDetection } from '@an
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Routes, provideRouter, withHashLocation } from '@angular/router';
 import { AppComponent } from '@app/pages/app/app.component';
+import { CreationPageComponent } from '@app/pages/creation-page/creation-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -18,6 +20,7 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'creation', component: CreationPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
