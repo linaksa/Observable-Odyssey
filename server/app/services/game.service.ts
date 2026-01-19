b4cb3cc835d960cc19e54d183e009eb9adde757a
Jeremie Lestage <jeremielestage@yahoo.ca>
2026-01-19 16:55:34 -0500

    FC - Modification, update and delete routes advanced

--- server/app/services/game.service.ts
+++ server/app/services/game.service.ts
@@ -43,4 +43,26 @@ export class GameService {
             throw new Error('Le terrain de jeu est invalide');
         }
     }
+
+    async updateGame(id: string, gameData: unknown): Promise<unknown> {
+        throw new Error('Not implemented');
+    }
+
+    async deleteGame(gameId: string): Promise<void> {
+        const existingGame = await game.findById(gameId);
+        if (!existingGame) {
+            throw new Error('jeu a déja été supprimé');
+        }
+        await game.deleteOne({ _id: gameId });
+    }
+
+    async changeVisibility(id: string, visibility: Visibility): Promise<IGame> {
+        const existingGame = await game.findById(id);
+        if (!existingGame) {
+            throw new Error('Jeu introuvable');
+        }
+        existingGame.visibility = visibility;
+        existingGame.lastModifiedDate = new Date();
+        return existingGame.save();
+    }
 }
