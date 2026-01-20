f9e41f66882e67d5b07726dce743462b4609a5e5
Victor Paul-Hus-Cote <victor.paul-hus-cote@outlook.com>
2026-01-20 13:10:20 -0500

    feature: page d'attente finalisé

--- client/src/app/pages/wait-page/wait-page.component.ts
+++ client/src/app/pages/wait-page/wait-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Router } from '@angular/router';
+import { AppMaterialModule } from '@app/modules/material.module';
 
 @Component({
     selector: 'app-wait-page',
-    imports: [MatProgressSpinnerModule],
+    imports: [AppMaterialModule, MatProgressSpinnerModule],
     templateUrl: './wait-page.component.html',
     styleUrl: './wait-page.component.scss',
 })
-export class WaitPageComponent {}
+export class WaitPageComponent {
+    timeToShowButtonMs: number = 3000;
+    showButton: boolean = false;
+
+    constructor(private router: Router) {
+        setTimeout(() => (this.showButton = true), this.timeToShowButtonMs);
+    }
+
+    goHome(): void {
+        this.router.navigateByUrl('/home');
+    }
+}
