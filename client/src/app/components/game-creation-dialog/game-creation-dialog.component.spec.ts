4d8270367a5850b70316acedd00178caf853cdcf
FelixTed <cadieuxfelix@outlook.com>
2026-01-23 11:44:39 -0500

    FC - added create button

--- client/src/app/components/game-creation-dialog/game-creation-dialog.component.spec.ts
+++ client/src/app/components/game-creation-dialog/game-creation-dialog.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameCreationDialogComponent } from './game-creation-dialog.component';
 
 describe('GameCreationDialogComponent', () => {
-  let component: GameCreationDialogComponent;
-  let fixture: ComponentFixture<GameCreationDialogComponent>;
+    let component: GameCreationDialogComponent;
+    let fixture: ComponentFixture<GameCreationDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GameCreationDialogComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [GameCreationDialogComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(GameCreationDialogComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+        fixture = TestBed.createComponent(GameCreationDialogComponent);
+        component = fixture.componentInstance;
+        await fixture.whenStable();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
