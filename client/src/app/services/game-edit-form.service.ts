477a2d54b7c0261c8d4306a0ff786b53786b8d21
Thomas Pronovost <thomas.pronovost@polymtl.ca>
2026-02-06 16:37:08 +0000

    Hotfix/refactor grid

--- client/src/app/services/game-edit-form.service.ts
+++ client/src/app/services/game-edit-form.service.ts
@@ -8,15 +8,15 @@ import html2canvas from 'html2canvas-oklch';
 import { GameService } from './game.service';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class GameEditFormService {
-    gameService = inject(GameService);
+  gameService = inject(GameService);
 
-    form: FormGroup;
-    formValid: boolean = false;
-    formErrors: string[];
-    isSubmitting: WritableSignal<boolean> = signal(false);
+  form: FormGroup;
+  formValid: boolean = false;
+  formErrors: string[];
+  isSubmitting: WritableSignal<boolean> = signal(false);
 
     customHtml2Canvas = html2canvas;
 
@@ -34,79 +34,81 @@ export class GameEditFormService {
         });
     }
 
-    init(gameData: EditGameFormData): void {
-        this.form.patchValue({
-            gameTitle: gameData.gameTitle,
-            description: gameData.description,
-            gameMode: gameData.gameMode,
-        });
+  init(gameData: EditGameFormData): void {
+    this.form.patchValue({
+      gameTitle: gameData.gameTitle,
+      description: gameData.description,
+      gameMode: gameData.gameMode,
+    });
+  }
+
+  async getPreviewImage(gridSelector: HTMLElement | null): Promise<Base64URLString | null> {
+    if (!gridSelector) {
+      return null;
     }
 
-    async getPreviewImage(gridSelector: HTMLElement | null): Promise<Base64URLString | null> {
-        if (!gridSelector) {
-            return null;
-        }
+    let imgData: Base64URLString;
+    try {
+      const canvas: HTMLCanvasElement = await this.customHtml2Canvas(gridSelector);
+      imgData = canvas.toDataURL('image/png');
+    } catch {
+      return null;
+    }
+    return imgData;
+  }
 
-        let imgData: Base64URLString;
-        try {
-            const canvas: HTMLCanvasElement = await this.customHtml2Canvas(gridSelector);
-            imgData = canvas.toDataURL('image/png');
-        } catch {
-            return null;
-        }
-        return imgData;
+  async submitForm(id: string, cells: CellType[][], items: IItem[], gridSelector: HTMLElement | null): Promise<void> {
+    this.isSubmitting.set(true);
+    // Allow angular to rerender before html2canva blocks the cycle somehow
+    await new Promise(resolve => setTimeout(resolve, 0));
+    this.formErrors = [];
+
+    const previewImage = await this.getPreviewImage(gridSelector);
+    if (!previewImage) {
+      this.formValid = false;
+      this.formErrors = ['Une erreur est survenue lors de la génération de l\'aperçu du plateau.'];
+      this.isSubmitting.set(false);
+      return Promise.reject();
     }
 
-    async submitForm(id: string, cells: CellType[][], items: IItem[], gridSelector: HTMLElement | null): Promise<void> {
-        this.isSubmitting.set(true);
-        // Allow angular to rerender before html2canva blocks the cycle somehow
-        await new Promise((resolve) => setTimeout(resolve, 0));
-        this.formErrors = [];
+    const formData = this.form.value;
+    const board: IBoard = {
+      cells,
+      items,
+    };
 
-        const previewImage = await this.getPreviewImage(gridSelector);
-        if (!previewImage) {
-            this.formValid = false;
-            this.formErrors = ["Une erreur est survenue lors de la génération de l'aperçu du plateau."];
+    const gameData: EditGameFormData = {
+      gameTitle: formData.gameTitle,
+      description: formData.description,
+      gameMode: formData.gameMode,
+      preview: previewImage,
+      board,
+    };
+
+    let observable;
+    if (id) {
+      observable = this.gameService.saveGame(id, gameData);
+    } else {
+      observable = this.gameService.createGame(gameData);
+    }
+
+    return new Promise(
+      (resolve, reject) => {
+        observable.subscribe({
+          next: () => {
+            this.formValid = true;
             this.isSubmitting.set(false);
-            return Promise.reject();
-        }
-
-        const formData = this.form.value;
-        const board: IBoard = {
-            cells,
-            items,
-        };
-
-        const gameData: EditGameFormData = {
-            gameTitle: formData.gameTitle,
-            description: formData.description,
-            gameMode: formData.gameMode,
-            preview: previewImage,
-            board,
-        };
-
-        let observable;
-        if (id) {
-            observable = this.gameService.saveGame(id, gameData);
-        } else {
-            observable = this.gameService.createGame(gameData);
-        }
-
-        return new Promise((resolve, reject) => {
-            observable.subscribe({
-                next: () => {
-                    this.formValid = true;
-                    this.isSubmitting.set(false);
-                    resolve();
-                },
-                error: (err) => {
-                    this.formValid = false;
-                    const errors = JSON.parse(err.error);
-                    this.formErrors = ['Une erreur est survenue lors de la sauvegarde du jeu.', errors.error];
-                    this.isSubmitting.set(false);
-                    reject();
-                },
-            });
+            resolve();
+          },
+          error: (err) => {
+            err = JSON.parse(err.error);
+            this.formValid = false;
+            this.formErrors = ['Une erreur est survenue lors de la sauvegarde du jeu.', err.error];
+            this.isSubmitting.set(false);
+            reject();
+          },
         });
-    }
+      },
+    );
+  }
 }
